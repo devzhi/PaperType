7ae7b356bbe2e65e2c47c2d5320a5e38e83c177a
hiyelang <ilaozheng@outlook.com>
2017-06-16 11:20:17 +0800

    增加后台缩略图和返回顶部按钮

--- footer.php
+++ footer.php
@@ -11,4 +11,10 @@
 </body>
 	<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="<?php $this->options->themeUrl('js/jquery.goup.min.js'); ?>"></script>
+	<script>
+	jQuery(document).ready(function(){
+	    jQuery.goup();
+	});
+	</script>
 </html>
