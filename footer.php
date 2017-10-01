f7769184303a5ed8b16ca9b46344c3424dbb879d
= <=>
2017-10-01 22:32:48 +0800

    add instantclick.min.js

--- footer.php
+++ footer.php
@@ -10,6 +10,8 @@
 	</div>
 </body>
 	<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+	<script src="<?php $this->options->themeUrl('js/instantclick.min.js'); ?>" data-no-instant></script>
+	<script data-no-instant>InstantClick.init();</script>
 	<script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<script src="<?php $this->options->themeUrl('js/jquery.goup.min.js'); ?>"></script>
 	<script>
